a9d4e46b7d7963e3d63a1d84e7012125d23976d0
Merlin Rock <rock.merlin@yahoo.com>
2021-02-05 18:52:15 -0500

    final code refactor

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Header from "./components/header";
-import Wrapper from "./components/wrapper"
 import SearchResult from "./components/search-results"
 
 
 function App() {
   return (
     <div className="app">
-      {/* <Wrapper> */}
       <Header />
       <SearchResult />
-      {/* </Wrapper> */}
     </div>
   );
 }
